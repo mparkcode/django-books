b4b9c751ca3a9ad3464415ffc86467657f595f6d
Michael Park <mparkcode@gmail.com>
2018-06-14 15:50:48 +0000

    Added add a book function and displayed according to owner

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,4 +10,8 @@ class Book(models.Model):
     isbn = models.CharField(max_length=254, blank=False)
     notes = models.TextField(blank=False)
     image = models.ImageField(upload_to='images', default="images/default.png")
+    owner = models.ForeignKey(User, related_name="books", null=False)
+    
+    def __str__(self):
+        return self.name
    
--- books/views.py
+++ books/views.py
@@ -5,14 +5,19 @@ from .forms import AddBookForm
 
 # Create your views here.
 def get_index(request):
-    books = Book.objects.all()
+    if request.user.is_authenticated:
+        books = Book.objects.filter(owner=request.user)
+    else:
+        books = []
     return render(request, "index.html", {'books':books})
     
 def add_book(request):
     if request.method=="POST":
         form=AddBookForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            book = form.save(commit=False)
+            book.owner = request.user
+            book.save()
         return redirect("/")
     else:
         form = AddBookForm()
